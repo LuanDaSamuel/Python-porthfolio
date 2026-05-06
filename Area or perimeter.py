f04d2deae0203163fcfe58b3d73c10390207895d
SinevaDeCoder <minhluan.sam@gmail.com>
2026-05-06 10:44:03 +0300

    Refactor area and perimeter calculation logic

--- Area or perimeter.py
+++ Area or perimeter.py
@@ -1,3 +1,4 @@
+#Final result
 r = float(input("Anna ympyrän säde: "))
 print("Lasketaanko ympyrän piiri (p) vai pinta-ala (a)?")
 print("Valitse p/a: ")
@@ -16,3 +17,23 @@ elif vastaus != "p" or vastaus != "a":
 else:
     print("Annoit virheellisen säteen arvon")
     print("Ohjelman suoritus päättyy.")
+
+#Bài của một khứa hôi lông trên MAOL2
+r = float(input("Anna ympyrän säde: "))
+print("Lasketaanko ympyrän piiri (p) vai pinta-ala (a)?")
+print("Valitse p/a: ")
+vastaus = input()
+if r > 0:
+if vastaus == "p":
+piiri = 2 * 3.14 * r
+print("Piiri:", round(piiri, 2))
+else:
+if vastaus == "a":
+ala = 3.14 * r ** 2
+print("Ala:", round(ala, 2))
+else:
+print("Annoit jonkin muun vastauksen kuin p tai a.")
+print("Ohjelman suoritus päättyy.")
+else:
+print("Annoit virheellisen säteen arvon")
+print("Ohjelman suoritus päättyy.")
