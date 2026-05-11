fc2329be909576c40af5f61d76f1deb6e0674f62
SinevaDeCoder <minhluan.sam@gmail.com>
2026-05-11 11:22:55 +0300

    Implement divisibility checks for numbers 1 to 100
    Fix loop increment and print statements for divisibility checks.

---
+++ from 1 to 100 divideable wth 3 and 5.py
@@ -0,0 +1,9 @@
+luku = 1
+while luku <= 100:
+    if luku % 3 == 0:
+        print("Luku", luku, "jaollinen 3, sen tulo on", luku / 3)
+    elif luku % 5 == 0:
+        print("Luku", luku, "jaollinen 5, sen tulo on", luku / 5)
+
+
+    luku += 1
